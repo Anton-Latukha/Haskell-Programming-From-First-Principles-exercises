c213ef69687f845714850f8b9aa3b8633f7a675e
Anton-Latukha <anton.latukha@gmail.com>
2018-02-09 02:02:50 +0200

    Ch5: add Chap Ex DetermineType 2-5

--- CHAPTER 5. TYPES/5.8.DetermineType.hs
+++ CHAPTER 5. TYPES/5.8.DetermineType.hs
@@ -17,3 +17,37 @@ c = head [(0,"doge"),(1 :: Integer,"kitteh")]
 d = if False then True else False
 e = length [1,2,3,4,5]
 f = (length [1,2,3,4]) > (length "TACOCAT")
+
+
+-- 2. Given
+w = y * 10 where
+    y = x + 5
+    x = 5
+
+-- What is the type of w?
+
+
+-- 3. Given
+y = x + 5 where
+    x = 5
+z y = y * 10
+    
+    
+-- What is the type of z?
+
+
+-- 4. Given
+f4 = 4 / y where
+    y = x + 5
+    x = 5
+
+-- What is the type of f?
+
+
+-- 5. Given
+f5 = x ++ y ++ z where
+    x = "Julie"
+    y = " <3 "
+    z = "Haskell"
+
+-- What is the type of f?
