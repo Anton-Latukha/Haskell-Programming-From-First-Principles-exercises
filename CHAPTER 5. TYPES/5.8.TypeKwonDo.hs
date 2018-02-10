c30c5e7400cc9a3f15764e6ae686802b02bbbf3e
Anton-Latukha <anton.latukha@gmail.com>
2018-02-10 17:32:14 +0200

    Ch5: add Chap Ex TypeKwonDo

---
+++ CHAPTER 5. TYPES/5.8.TypeKwonDo.hs
@@ -0,0 +1,53 @@
+module TypeKwonDo where
+
+
+-- 1.
+
+f :: Int -> String
+f = undefined
+
+g :: String -> Char
+g = undefined
+
+h :: Int -> Char
+h num = g (f num)
+
+
+-- 2.
+
+data A
+data B
+data C
+
+q :: A -> B
+q = undefined
+
+
+w :: B -> C
+w = undefined
+
+
+e :: A -> C
+e a = w (q a)
+
+
+-- 3.
+
+data X
+data Y
+data Z
+
+xz :: X -> Z
+xz = undefined
+
+yz :: Y -> Z
+yz = undefined
+
+xform :: (X, Y) -> (Z, Z)
+xform (x, y) = ((xz x), (yz y))
+
+
+-- 4.
+
+munge :: (x -> y) -> ((y -> (w, z)) -> x -> w)
+munge xToY yToTupWZ x = fst (yToTupWZ (xToY x))
