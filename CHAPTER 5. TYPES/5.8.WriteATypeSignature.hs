42a4dc66c3e6d8890c90a5253dda006e1df53248
Anton-Latukha <anton.latukha@gmail.com>
2018-02-11 02:03:37 +0200

    Ch5: add Chap Ex WriteTypeSignature

--- CHAPTER 5. TYPES/5.8.WriteATypeSignature.hs
+++ CHAPTER 5. TYPES/5.8.WriteATypeSignature.hs
@@ -1,11 +1,16 @@
 module WriteATypeSignature where
 
+-- 1.
 functionH :: [a] -> a
 functionH (x:_) = x
 
+
+-- 2.
 functionC :: Ord a => a -> a -> Bool
 functionC x y =
     if (x > y) then True else False
 
+
+-- 3.
 functionS :: (a, b) -> b
 functionS (x, y) = y
