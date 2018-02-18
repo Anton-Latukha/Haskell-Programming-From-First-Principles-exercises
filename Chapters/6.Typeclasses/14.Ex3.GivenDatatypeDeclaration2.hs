bc2e612bc576eba4bc5a9821b92843dbc1b800f8
Anton-Latukha <anton.latukha@gmail.com>
2018-02-18 21:53:25 +0200

    Ch6: add 14.Ex3.GivenDatatypeDeclaration2

---
+++ Chapters/6.Typeclasses/14.Ex3.GivenDatatypeDeclaration2.hs
@@ -0,0 +1,11 @@
+data Rocks =
+    Rocks String deriving (Eq, Show)
+
+data Yeah =
+    Yeah Bool deriving (Eq, Show)
+
+data Papu =
+    Papu Rocks Yeah deriving (Eq, Show)
+
+truth = Papu (Rocks "chomskydoz")
+             (Yeah True)
