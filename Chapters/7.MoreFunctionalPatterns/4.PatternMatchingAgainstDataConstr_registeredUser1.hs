dc12732a9677ffaf66034c7f8e46a8e7a1ec30dd
Anton-Latukha <anton.latukha@gmail.com>
2018-02-22 04:07:00 +0200

    Ch7: add 4.PatternMatchingAgainstDataConstr_registeredUser1

---
+++ Chapters/7.MoreFunctionalPatterns/4.PatternMatchingAgainstDataConstr_registeredUser1.hs
@@ -0,0 +1,13 @@
+module RegisteredUser where
+
+
+
+newtype Username =
+    Username String
+
+    
+newtype AccountNumber =
+    AccountNumber Integer
+    
+    
+data User = UnregisteredUser | RegisteredUser Username AccountNumber
