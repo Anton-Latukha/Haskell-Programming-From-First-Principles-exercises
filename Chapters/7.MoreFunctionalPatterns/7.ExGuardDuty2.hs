3d0c1fba4c67911126ce987a872675a0446879c5
Anton-Latukha <anton.latukha@gmail.com>
2018-03-05 19:05:42 +0200

    Ch7: add 7.ExGuardDuty2

---
+++ Chapters/7.MoreFunctionalPatterns/7.ExGuardDuty2.hs
@@ -0,0 +1,9 @@
+avgGrade :: (Fractional a, Ord a) => a -> Char
+avgGrade x
+    | y >= 0.7 = 'C'
+    | y >= 0.9 = 'A'
+    | y >= 0.8 = 'B'
+    | y >= 0.59 = 'D'
+    | True = 'F'
+    where y = x / 100
+    
