c3720aa9789b0f0983db373fdc47fbf4c0badaa7
Anton-Latukha <anton.latukha@gmail.com>
2018-03-13 18:04:09 +0200

    Ch7: add 11.Let\'sWriteCode1PointFree

--- Chapters/7.MoreFunctionalPatterns/11.Let'sWriteCode1PointFree.hs
+++ Chapters/7.MoreFunctionalPatterns/11.Let'sWriteCode1PointFree.hs
@@ -1,4 +1,5 @@
 hundredsDigit :: Integral a => a -> a
 hundredsDigit = snd.flip divMod 10 . fst.flip divMod 100 . abs
+-- Fully pointfree version, for gymnastics
 -- divMod must be piped into itself to make mod.div
 -- div always rounds down to infinity, so abs for negative values working properly.
