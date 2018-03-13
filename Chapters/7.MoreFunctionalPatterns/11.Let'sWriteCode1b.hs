551c9305916150c3e788dcb3a0e382ee56723a1d
Anton-Latukha <anton.latukha@gmail.com>
2018-03-13 18:03:55 +0200

    Ch7: add 11.Let\'sWriteCode1b

--- Chapters/7.MoreFunctionalPatterns/11.Let'sWriteCode1b.hs
+++ Chapters/7.MoreFunctionalPatterns/11.Let'sWriteCode1b.hs
@@ -1,4 +1,4 @@
-tensDigit :: Integral a => a -> a
-tensDigit x = snd (divMod (fst (divMod (abs x) 100)) 10)
+hundredsDigit :: Integral a => a -> a
+hundredsDigit x = snd (divMod (fst (divMod (abs x) 100)) 10)
 -- divMod must be piped into itself to make mod.div
 -- div always rounds down to infinity, so abs for negative values working properly.
