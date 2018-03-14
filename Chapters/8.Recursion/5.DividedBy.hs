2c86b17775193cb7599e374881773a0437063ff9
Anton-Latukha <anton.latukha@gmail.com>
2018-03-14 15:45:26 +0200

    Ch8: add 5.DividedBy

---
+++ Chapters/8.Recursion/5.DividedBy.hs
@@ -0,0 +1,7 @@
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+    where go n d count
+           | n < d = (count, n)
+           | True =
+               go (n - d) d (count + 1)
+
