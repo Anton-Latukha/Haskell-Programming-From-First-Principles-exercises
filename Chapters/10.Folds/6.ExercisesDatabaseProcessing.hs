44d3f16b8c5fffaf392145971af7a42872b46978
Anton-Latukha <anton.latukha@gmail.com>
2018-03-18 18:53:21 +0200

    Ch9: add 6.ExercisesDatabaseProcessing1

---
+++ Chapters/10.Folds/6.ExercisesDatabaseProcessing.hs
@@ -0,0 +1,17 @@
+import Data.Time 
+
+data DatabaseItem = DbString String
+                  | DbNumber Integer
+                  | DbDate UTCTime
+                  deriving (Eq, Ord, Show)
+
+theDatabase :: [DatabaseItem]
+theDatabase =
+    [ DbDate (UTCTime
+              (fromGregorian 1911 5 1)
+      (secondsToDiffTime 34123))
+    , DbNumber 9001
+    , DbString "Hello, world!"
+    , DbDate (UTCTime
+              (fromGregorian 1921 5 1)
+              (secondsToDiffTime 34123))]
