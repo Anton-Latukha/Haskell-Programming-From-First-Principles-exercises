0461af705db9b55354c63d252e295592ea3aa81c
Anton-Latukha <anton.latukha@gmail.com>
2018-03-23 23:01:15 +0200

    Ch8: upd 4.Fibonacci

--- Chapters/8.Recursion/4.Fibonacci.hs
+++ Chapters/8.Recursion/4.Fibonacci.hs
@@ -1,4 +1,7 @@
-fibonacci :: Integer a => a -> a
+module Fibonacci where
+
+
+fibonacci :: (Eq a, Num a, Num t) => a -> t
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci (n - 2) + fibonacci (n - 1)
