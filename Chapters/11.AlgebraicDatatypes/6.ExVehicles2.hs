bf5928d0eb989983219fb9e9bc23f752d0a9edb3
Anton-Latukha <anton.latukha@gmail.com>
2018-03-24 22:56:04 +0200

    Ch11: add 5.ExVehicles2

---
+++ Chapters/11.AlgebraicDatatypes/6.ExVehicles2.hs
@@ -0,0 +1,28 @@
+data Price = Price Integer
+    deriving (Eq, Show)
+
+data Manufacturer = Mini | Mazda | Tata
+    deriving (Eq, Show)
+
+data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited
+    deriving (Eq, Show)
+
+data Vehicle = Car Manufacturer Price | Plane Airline
+    deriving (Eq, Show)
+
+
+myCar = Car Mini (Price 14000)
+urCar = Car Mazda (Price 20000)
+clownCar = Car Tata (Price 7000)
+doge = Plane PapuAir
+
+isCar :: Vehicle -> Bool
+isCar (Car _ _) = True
+isCar (Plane _) = False
+
+isPlane :: Vehicle -> Bool
+isPlane (Plane _) = True
+isPlane (Car _ _) = False
+
+areCars :: [Vehicle] -> [Bool]
+areCars list = fmap (isCar) list
