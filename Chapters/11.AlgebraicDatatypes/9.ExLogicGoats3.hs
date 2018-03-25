ad8110908652fce5714bfa47059e57c1567c3f4d
Anton-Latukha <anton.latukha@gmail.com>
2018-03-25 23:49:41 +0300

    Ch11: add 9.ExLogicGoats3

---
+++ Chapters/11.AlgebraicDatatypes/9.ExLogicGoats3.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+
+class TooMany a where
+    tooMany :: a -> Bool
+
+instance TooMany Int where
+    tooMany n = n > 42
+
+newtype Goats =
+    Goats Int deriving (Eq, Show, TooMany)
+
+data CountyStringy = TooMany (Int, String) deriving (Eq, Show)
+instance TooMany (Int, String) where
+    tooMany (int, _) = int > 42
+
+instance TooMany (Int, Int) where
+    tooMany (int1, int2) = (int1 + int2) > 42
+
+instance (Num a, TooMany a) => TooMany (a, a) where
+    tooMany (int1, int2) => tooMany (int1 + int2)
