6c8ff2ea5895df6f8f4c78a2bdfc9f9ab2892129
Anton-Latukha <anton.latukha@gmail.com>
2018-03-27 23:18:57 +0300

    Ch11: upd 13.ExProgrammers

--- Chapters/11.AlgebraicDatatypes/13.ExProgrammers.hs
+++ Chapters/11.AlgebraicDatatypes/13.ExProgrammers.hs
@@ -8,8 +8,31 @@ data ProgLang = Haskell
               | Agda
               | Idris
               | PureScript
-deriving (Eq, Show)
-data Programmer =
-Programmer { os :: OperatingSystem
-, lang :: ProgLang }
-deriving (Eq, Show)
+            deriving (Eq, Show)
+
+data Programmer = Programmer { os :: OperatingSystem
+                             , lang :: ProgLang }
+                            deriving (Eq, Show)
+
+nineToFive :: Programmer
+nineToFive = Programmer { os = Mac
+                        , lang = Haskell }
+                        -- We can reorder stuff
+                        -- when we use record syntax
+feelingWizardly :: Programmer
+feelingWizardly = Programmer { lang = Agda
+                             , os = GnuPlusLinux }
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+    [ GnuPlusLinux
+    , OpenBSDPlusNevermindJustBSDStill
+    , Mac
+    , Windows
+    ]
+
+allLanguages :: [ProgLang]
+allLanguages = [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [OperatingSystem] -> [ProgLang] -> [Programmer]
+allProgrammers oses langs = fmap (\ x -> Programmer fst(x) snd(x)) (zipWith (,) oses langs)
