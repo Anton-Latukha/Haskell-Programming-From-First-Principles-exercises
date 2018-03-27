295faf0988714404b830ffe33eff822df20aff61
Anton-Latukha <anton.latukha@gmail.com>
2018-03-27 23:29:36 +0300

    Ch11: upd 13.ExProgrammers

--- Chapters/11.AlgebraicDatatypes/13.ExProgrammers.hs
+++ Chapters/11.AlgebraicDatatypes/13.ExProgrammers.hs
@@ -14,15 +14,6 @@ data Programmer = Programmer { os :: OperatingSystem
                              , lang :: ProgLang }
                             deriving (Eq, Show)
 
-nineToFive :: Programmer
-nineToFive = Programmer { os = Mac
-                        , lang = Haskell }
-                        -- We can reorder stuff
-                        -- when we use record syntax
-feelingWizardly :: Programmer
-feelingWizardly = Programmer { lang = Agda
-                             , os = GnuPlusLinux }
-
 allOperatingSystems :: [OperatingSystem]
 allOperatingSystems =
     [ GnuPlusLinux
