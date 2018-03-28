4bc3dd61022aa4e3f4f47772e5fe8ba3d33b1097
Anton-Latukha <anton.latukha@gmail.com>
2018-03-28 22:50:50 +0300

    Ch11: add 14.ExTheQuad1.hs

---
+++ Chapters/11.AlgebraicDatatypes/14.ExTheQuad1.hs
@@ -0,0 +1,11 @@
+import Data.Cardinality
+
+data Quad = One
+          | Two
+          | Three
+          | Four
+          deriving (Eq, Show)
+
+-- 1. eQuad :: Either Quad Quad
+-- Left side has Left constructor, and right side has Right constructor, so they give different result
+e1EQuad = 4 + 4
