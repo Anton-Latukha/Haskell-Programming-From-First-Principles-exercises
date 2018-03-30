efa3290173c9b1b1c8c94ab47cbc97de42297f6d
Anton-Latukha <anton.latukha@gmail.com>
2018-03-30 14:48:20 +0300

    Ch11: upd 17.ConvertBinaryTreesToLists.hs

--- Chapters/11.AlgebraicDatatypes/17.ConvertBinaryTreesToLists.hs
+++ Chapters/11.AlgebraicDatatypes/17.ConvertBinaryTreesToLists.hs
@@ -1,34 +1,30 @@
+import Data.List
+
 data BinaryTree a =
     Leaf
     | Node (BinaryTree a) a (BinaryTree a)
                   deriving (Eq, Ord, Show)
-
-mapTree :: (a -> b)
-        -> BinaryTree a
-        -> BinaryTree b
-
-mapTree _ Leaf = Leaf
-mapTree f (Node left a right) = Node (mapTree f left) (f a) (mapTree f right)
-
-testTree' :: BinaryTree Integer
-testTree' =
-  Node (Node Leaf 3 Leaf)
-       1
-       (Node Leaf 4 Leaf)
-
-mapExpected =
-  Node (Node Leaf 4 Leaf)
-       2
-       (Node Leaf 5 Leaf)
-
--- acceptance test for mapTree
-mapOkay =
-  if mapTree (+1) testTree' == mapExpected
-  then print "yup okay!"
-  else error "test failed!"
 
 treeToList :: BinaryTree a -> [a]
 treeToList tree = go tree []
   where
     go Leaf list = list
     go (Node left a right) list = ((go left []) ++ [a] ++ (go right []))
+
+testTree :: BinaryTree Integer
+testTree =
+  Node (Node Leaf 1 Leaf)
+  2
+  (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder =
+  if treeToList testTree == [1, 2, 3]  -- If binary tree is holistic, a binary search, algorythm returns sorted list.
+  then putStrLn "Preorder fine!"
+  else putStrLn "Bad news bears."
+
+testInorder :: IO ()
+testInorder =
+  if sort (treeToList testTree) == [1, 2, 3]
+  then putStrLn "Inorder fine!"
+  else putStrLn "Bad news bears."
