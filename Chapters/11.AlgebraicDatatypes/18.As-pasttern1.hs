f730840030b262ee38052a1e24545ba0dff03f58
Anton-Latukha <anton.latukha@gmail.com>
2018-03-31 18:01:18 +0300

    Ch11: upd 18.As-pasttern1.hs

---
+++ Chapters/11.AlgebraicDatatypes/18.As-pasttern1.hs
@@ -0,0 +1,10 @@
+-- The trick is by requirement we need not match by substring or chars, but presence of chars in the list in the order, 'h' before 'b','l','a' would not match.
+
+import Data.List
+
+isSubseqOf :: (Eq a) => [a] -> [a] -> Bool
+isSubseqOf input@(i:is) set@(s:ss)
+  | is == [] && elem i set = True
+  | set == [] = False
+  | elem i set = isSubseqOf is (tail (dropWhile (/=i) set))
+  | True = False
