918eacc447eea8951de8dc5cf2350f133ff0b600
Anton-Latukha <anton.latukha@gmail.com>
2018-04-05 14:54:15 +0300

    Ch11: upd 18.LanguageEx1.hs

--- Chapters/11.AlgebraicDatatypes/18.LanguageEx1.hs
+++ Chapters/11.AlgebraicDatatypes/18.LanguageEx1.hs
@@ -1,4 +1,3 @@
--- The trick is by requirement we need not match by substring or chars, but presence of chars in the list in the order, 'h' before 'b','l','a' would not match.
 import Data.Char
 
 capitalizeWord :: String -> String
