79eb18cd0a2020791418163328f61a3a100d7934
Anton-Latukha <anton.latukha@gmail.com>
2018-04-05 22:39:55 +0300

    Ch11: upd 18.PhoneExercise1.hs

--- Chapters/11.AlgebraicDatatypes/18.PhoneExercise1.hs
+++ Chapters/11.AlgebraicDatatypes/18.PhoneExercise1.hs
@@ -14,14 +14,20 @@ type Button = Char
 type Values = String
 data Phone = Phone [(Button, Values)]
 
+phoneButtons :: [Button]
 phoneButtons = "*#0123456789"
+phoneCommands :: [Values]
 phoneCommands = ["^*", ".,#", " +_0", "1", "abc2", "def3", "ghi4", "jkl5", "mno6", "pqrs7", "tuv8", "wxyz9"]
 
-phoneDataModel = zip phoneButtons phoneCommands
+-- If lists of buttons and commands have same number of indexes - create a consistent data model
+phoneDataModel :: [Button] -> [Values] -> Phone
+phoneDataModel phoneButtons phoneCommands
+  | length phoneButtons == length phoneCommands = Phone (zip phoneButtons phoneCommands)
+  | True = Phone []
 
 -- Validate sequence element as a legitimate button
-validButton :: Phone -> Button -> Bool
-validButton phone button = elem button phoneButtons
+validButton :: [Button] -> Button -> Bool
+validButton phoneButtons button = elem button phoneButtons
 
 -- Takes sequence of button presses and returnes list of subsequent clusters of the same presses
 splitToSameTaps :: [Button] -> [[Button]]
@@ -33,10 +39,12 @@ splitToSameTaps str = sameTapsCluster ++ reminder
     reminder = splitToSameTaps (drop (length sameTaps) str)
 
 -- Translate cluster of the same button presses into according command
-sameTaps :: Phone -> [Button] -> [Char]
-sameTaps phone str = go phone str (elemIndex (head str) phoneButtons)
+sameTaps :: [Button] -> [Button] -> [Values] -> [Char]
+sameTaps phoneButtons code phoneCommands = go phoneCommands code (elemIndex (head code) phoneButtons)
   where
     go _ _ Nothing = []
-    go phone str (Just index)
-      | head str == '1' = str
+    go phoneCommands code (Just index)
+      -- As 1 has one value, and as phones did - return 1 as many times as it was pressed.
+      | head code == '1' = code
+      -- Get the command by matching the number of the same presses in the cluster to the infinite loop of command values
       | True = [cycle (phoneCommands !! index) !! (length str-1)]
