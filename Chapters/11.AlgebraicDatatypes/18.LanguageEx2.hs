5dd3fb8f0a8e425288eeb31dcf5067b6219b4df6
Anton-Latukha <anton.latukha@gmail.com>
2018-04-05 14:54:20 +0300

    Ch11: upd 18.LanguageEx2.hs

--- Chapters/11.AlgebraicDatatypes/18.LanguageEx2.hs
+++ Chapters/11.AlgebraicDatatypes/18.LanguageEx2.hs
@@ -1,11 +1,8 @@
--- The trick is by requirement we need not match by substring or chars, but presence of chars in the list in the order, 'h' before 'b','l','a' would not match.
 import Data.Char
-
 
 capitalizeWord :: String -> String
 capitalizeWord [] = []
 capitalizeWord (w:rd) = [(toUpper w)]++rd
-
 
 capitalizeSentences :: String -> String
 capitalizeSentences [] = []
@@ -14,5 +11,5 @@ capitalizeSentences ('.':ext) = "." ++ (capitalizeSentences ext) -- Passthrough 
 capitalizeSentences (' ':ext) = " " ++ (capitalizeSentences ext) -- Passthrough ' '
 capitalizeSentences text = (capitalizeWord fSen) ++ (capitalizeSentences (drop (length fSen) text))
   where
-      fSen = takeWhile (/='.') text
+    fSen = takeWhile (/='.') text
 
