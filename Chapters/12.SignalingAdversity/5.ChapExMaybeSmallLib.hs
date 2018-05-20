b7fa191b2dd0988ca07b8165f36bfb2e5ddb7715
Anton-Latukha <anton.latukha@gmail.com>
2018-05-20 21:00:17 +0300

    Ch12: upd 5.ChapExMaybeSmallLib.hs

--- Chapters/12.SignalingAdversity/5.ChapExMaybeSmallLib.hs
+++ Chapters/12.SignalingAdversity/5.ChapExMaybeSmallLib.hs
@@ -5,9 +5,13 @@ isJust (Just a) = True
 isJust Nothing = False
 
 isNothing :: Maybe a -> Bool
-isNothing = not isJust
+isNothing = not.isJust
 
 -- 2. Maybe catamorphism
 mayybee :: b -> (a->b) -> Maybe a -> b
 mayybee b _ Nothing = b
 mayybee _ f (Just a) = f a
+
+-- 3. Fallback value
+fromMaybe :: a -> Maybe a -> a
+fromMaybe fallback maybe = mayybee fallback (id) maybe
