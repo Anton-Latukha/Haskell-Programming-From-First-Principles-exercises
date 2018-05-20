e518c22da780655ff671e0a5d8fefa3641648a1c
Anton-Latukha <anton.latukha@gmail.com>
2018-05-20 20:55:17 +0300

    Ch12: upd 5.ChapExMaybeSmallLib.hs

--- Chapters/12.SignalingAdversity/5.ChapExMaybeSmallLib.hs
+++ Chapters/12.SignalingAdversity/5.ChapExMaybeSmallLib.hs
@@ -6,3 +6,8 @@ isJust Nothing = False
 
 isNothing :: Maybe a -> Bool
 isNothing = not isJust
+
+-- 2. Maybe catamorphism
+mayybee :: b -> (a->b) -> Maybe a -> b
+mayybee b _ Nothing = b
+mayybee _ f (Just a) = f a
