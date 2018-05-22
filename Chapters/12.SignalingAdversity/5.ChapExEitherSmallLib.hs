bea085a95493628d36718bc16552afc94f6cd81b
Anton-Latukha <anton.latukha@gmail.com>
2018-05-22 10:59:27 +0300

    Ch12: upd 5 5.ChapExEitherSmallLib.hs

--- Chapters/12.SignalingAdversity/5.ChapExEitherSmallLib.hs
+++ Chapters/12.SignalingAdversity/5.ChapExEitherSmallLib.hs
@@ -4,16 +4,16 @@
 lefts' :: [Either a b] -> [a]
 lefts' eitherList = foldr (go) [] eitherList
   where
-    go (Left left) silos = left:silos
-    go _           silos = silos
+    go (Left left) silo = left:silo
+    go _           silo = silo
 
 
 -- 2. Get right
 rights' :: [Either a b] -> [b]
 rights' eitherList = foldr (go) [] eitherList
   where
-    go (Right right) silos = right:silos
-    go _             silos = silos
+    go (Right right) silo = right:silo
+    go _             silo = silo
 
 
 -- 3. Sort lefts and rights to separate lists
@@ -29,3 +29,9 @@ partitionEithers' eitherList = foldr (go) ([],[]) eitherList
 eitherMaybe' :: (b -> c) -> Either a b -> Maybe c
 eitherMaybe' f (Right b) = Just (f b)
 eitherMaybe' _ _ = Nothing
+
+
+-- 5. General catamorphism for Either values
+either' :: (a -> c) -> (b -> c) -> Either a b -> c
+either' fac _ (Left a) = fac a
+either' _ fbc (Right b) = fbc b
