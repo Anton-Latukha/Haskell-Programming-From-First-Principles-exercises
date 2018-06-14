044753b622e4febba2105548afd13ecd14e66530
Anton-Latukha <anton.latukha@gmail.com>
2018-06-14 17:00:59 +0300

    Ch13: hello: add a prompt

--- Chapters/13.BuildingProjects/hello/src/Main.hs
+++ Chapters/13.BuildingProjects/hello/src/Main.hs
@@ -2,9 +2,12 @@ module Main where
 
 import DogsRule
 import Hello
+import System.IO
 
 main :: IO ()
 main = do
+  hSetBuffering stdout NoBuffering -- so that putStr isn't burrefed
+  putStr "Please input your name: "
   name <- getLine
   sayHello name
   dogs
