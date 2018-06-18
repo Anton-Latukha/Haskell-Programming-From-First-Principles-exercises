ecdbade33c9d027ce65004e4c89f96ac8e694d93
Anton-Latukha <anton.latukha@gmail.com>
2018-06-18 20:11:37 +0300

    Ch13: 10: upd hangman proj Main.hs

--- Chapters/13.BuildingProjects/hangman/src/Main.hs
+++ Chapters/13.BuildingProjects/hangman/src/Main.hs
@@ -6,3 +6,34 @@ import Data.Maybe (isJust)
 import Data.List (intersperse)
 import System.Exit (exitSuccess)
 import System.Random (randomRIO)
+
+type WordList = [String]
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
+
+minWordLength :: Int
+minWordLength = 4
+
+maxWordLength :: Int
+maxWordLength = 8
+
+gameWords :: IO WordList
+gameWords = do
+  aw <- allWords
+  return (filter gameLength aw)
+  where gameLength w =
+          let l = length (w ::string )
+          in     l >= minWordLength
+              && l <  maxWordLength
+
+randomWord :: WordLit -> IO String
+randomWord = do
+  randomIndex <- randomRIO (0,7775)
+  return $ wl !! randomIndex
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
+
