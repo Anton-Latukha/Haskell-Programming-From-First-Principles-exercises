4f12e7600037739732e83d61d177ca3926dae4da
Anton-Latukha <anton.latukha@gmail.com>
2018-06-26 00:37:53 +0300

    Ch13: 14: upd Ch11:

--- Chapters/11.AlgebraicDatatypes/18.Chiphers.hs
+++ Chapters/11.AlgebraicDatatypes/18.Chiphers.hs
@@ -3,14 +3,14 @@ module Chipher where
 import Data.Char
 
 cipherCaesar :: String -> Int -> String
-cipherCaesar str n = map (\x -> chr ((mod (ord x + n - 97) 26) + 97)) str
+cipherCaesar str n = map (\x -> chr (mod (ord x + n - 97) 26 + 97)) str
 
 unChipherCaesar :: String -> Int -> String
-unChipherCaesar str n = map (\x -> chr ((mod (ord x - n - 97) 26) + 97)) str
+unChipherCaesar str n = map (\x -> chr (mod (ord x - n - 97) 26 + 97)) str
 
 -- Works for mixed capitalized, small letters and spaces, produces capitalized text with spaces as '@'.
 cipherVigenere :: String -> String -> String
-cipherVigenere text key = zipWith (\ t k -> chr ((mod ((ord t) - (ord k)) 32)+64)) text (cycle key)
+cipherVigenere text key = zipWith (\t k -> chr (mod (ord t - ord k) 32+64)) text (cycle key)
 
 unCipherVigenere :: String -> String -> String
-unCipherVigenere text key = zipWith (\ t k -> chr ((mod ((ord t) + (ord k)) 32)+64)) text (cycle key)
+unCipherVigenere text key = zipWith (\t k -> chr (mod (ord t + ord k) 32+64)) text (cycle key)
