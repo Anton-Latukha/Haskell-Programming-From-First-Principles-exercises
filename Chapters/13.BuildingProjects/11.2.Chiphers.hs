68f38545b4faba928375a1e4fbcb7cfcfba36bb1
Anton-Latukha <anton.latukha@gmail.com>
2018-06-27 15:26:15 +0300

    Ch13: 14: add 11.2.Chiphers.hs

---
+++ Chapters/13.BuildingProjects/11.2.Chiphers.hs
@@ -0,0 +1,40 @@
+module Chipher where
+
+import Data.Char (ord, chr)
+
+cipherCaesar :: String -> Int -> String
+cipherCaesar str n = map (\x -> chr (mod (ord x + n - 97) 26 + 97)) str
+
+unChipherCaesar :: String -> Int -> String
+unChipherCaesar str n = map (\x -> chr (mod (ord x - n - 97) 26 + 97)) str
+
+-- Works for mixed capitalized, small letters and spaces, produces capitalized text with spaces as '@'.
+cipherVigenere :: String -> String -> String
+cipherVigenere text key = zipWith (\t k -> chr (mod (ord t - ord k) 32+64)) text (cycle key)
+
+unCipherVigenere :: String -> String -> String
+unCipherVigenere text key = zipWith (\t k -> chr (mod (ord t + ord k) 32+64)) text (cycle key)
+
+ioCipherVigenere :: IO String
+ioCipherVigenere = do
+  text <- getLine
+  key <- getLine
+  return (zipWith (\t k -> chr (mod (ord t - ord k) 32+64)) text (cycle key))
+
+ioUnCipherVigenere :: IO String
+ioUnCipherVigenere = do
+  text <- getLine
+  key <- getLine
+  return (zipWith (\t k -> chr (mod (ord t + ord k) 32+64)) text (cycle key))
+
+ioCipherCaesar :: IO String
+ioCipherCaesar = do
+  str <- getLine
+  n <- getLine
+  return (map (\x -> chr (mod (ord x + (read n :: Int) - 97) 26 + 97)) str)
+
+ioUnCipherCaesar :: IO String
+ioUnCipherCaesar = do
+  str <- getLine
+  n <- getLine
+  return (map (\x -> chr (mod (ord x - (read n :: Int) - 97) 26 + 97)) str)
