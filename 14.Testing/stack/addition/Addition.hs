5ee2bea5a080ae88053bb73217a721f3372fcd62
Anton-Latukha <anton.latukha@gmail.com>
2018-08-28 13:54:37 +0300

    Ch14: stack: Addition: add Hspec test

--- 14.Testing/stack/addition/Addition.hs
+++ 14.Testing/stack/addition/Addition.hs
@@ -1,5 +1,9 @@
 -- Addition.hs
 module Addition where
 
-sayHello :: IO ()
-sayHello = putStrLn "hello!"
+import Test.Hspec
+
+main :: IO ()
+main = hspec $
+  describe "Addition" $
+    it "1 + 1 is greater than 1" $ 1 + 1 > 1 `shouldBe` True
