ed857df4e04d9bc90591a4dc89f1998d81bb629e
Anton-Latukha <anton.latukha@gmail.com>
2018-09-24 00:04:20 +0300

    Ch15: 10.ExOptionalMonoid: add main.nix

---
+++ 15.MonoidAndSemigroup/10.ExOptionalMonoid/main.nix
@@ -0,0 +1,29 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  d = { mkDerivation, base, stdenv, hspec }:
+    mkDerivation {
+      pname = "projectName";
+      version = "0.0.0.1";
+      src = ./src;
+      isLibrary = true;
+      isExecutable = true;
+      libraryHaskellDepends = [ base hspec ];
+      executableHaskellDepends = [ base hspec ];
+      homepage = "blog.latukha.com";
+      description = "Description";
+      license = stdenv.lib.licenses.bsd3;
+    };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage d {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
