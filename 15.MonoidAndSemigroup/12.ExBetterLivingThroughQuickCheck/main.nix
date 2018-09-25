3c27a351b476a475e4d3765e16fb695fd9f7859a
Anton-Latukha <anton.latukha@gmail.com>
2018-09-25 16:24:12 +0300

    Ch15: add 12.ExBetterLivingThroughQuickCheck: add main.nix, Main.lhs

---
+++ 15.MonoidAndSemigroup/12.ExBetterLivingThroughQuickCheck/main.nix
@@ -0,0 +1,29 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  d = { mkDerivation, base, stdenv }:
+    mkDerivation {
+      pname = "betterLivingThroughQuickCheck";
+      version = "0.0.0.1";
+      src = ./src;
+      isLibrary = true;
+      isExecutable = false;
+      libraryHaskellDepends = [ base ];
+      executableHaskellDepends = [ base ];
+      homepage = "blog.latukha.com";
+      description = "Description text";
+      license = stdenv.lib.licenses.bsd3;
+    };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage d {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
---
+++ 15.MonoidAndSemigroup/12.ExBetterLivingThroughQuickCheck/src/Main.lhs
@@ -0,0 +1,6 @@
+\begin{code}
+{-# LANGUAGE UnicodeSyntax #-}
+module ModuleName where
+
+main = print "Hello from the main function!"
+\end{code}
